09d9dbe541f8856b0539fa8e643dfcf0c912bb19
ssusa2 <audgns9207@naver.com>
2022-02-17 14:27:05 +0900

    Add: Main, Login 페이지 추가

--- src/pages/suinJung/Login/LoginJung.js
+++ src/pages/suinJung/Login/LoginJung.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './LoginJung.scss';
 
 function LoginJung() {
-  return <h1>Login Check!</h1>;
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate('/main-jung');
+  };
+  return (
+    <>
+      <div className="loginContainer">
+        <main className="loginBox">
+          <h1 className="logo">Westagram</h1>
+          <div className="loginText">
+            <form>
+              <input
+                id="name"
+                className="inputBox"
+                type="text"
+                placeholder="전화번호, 사용자 이름 또는 이메일"
+              />
+              <input
+                id="password"
+                className="inputBox"
+                type="password"
+                placeholder="비밀번호"
+              />
+              <button name="data" className="log-btn" onClick={goToMain}>
+                로그인
+              </button>
+            </form>
+          </div>
+          <p className="findPass">
+            <a href="#">비밀번호를 잊으셨나요?</a>
+          </p>
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default LoginJung;
--- src/pages/suinJung/Main/MainJung.js
+++ src/pages/suinJung/Main/MainJung.js
@@ -1,8 +1,184 @@
 import React from 'react';
 import Nav from '../../../components/Nav/Nav';
+import './MainJung.scss';
 
 function MainJung() {
-  return <Nav />;
+  return (
+    <>
+      <Nav />
+      <div className="mainPage">
+        <main>
+          <article>
+            <div className="feed-nav">
+              <div className="user">
+                <img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80\\" />
+                <h3>canon_mj</h3>
+              </div>
+              <div className="more">
+                <span>
+                  <img src="../../images/more.png" />
+                </span>
+              </div>
+            </div>
+            <div className="feed-image">
+              <img src="https://images.unsplash.com/photo-1616738227115-954748f35c61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+            </div>
+            <div className="feed-bottom">
+              <div className="interactiveicon">
+                <div className="icon-left">
+                  <i className="fas fa-heart"></i>
+                  <i className="fas fa-heart"></i>
+                  <i className="fas fa-heart"></i>
+                </div>
+                <img src="../../images/bookmark.png" />
+              </div>
+              <div className="feedlike">
+                <div className="likeimage">
+                  <img src="../../images/face.png" />
+                </div>
+                <p>
+                  <span>aineworld</span>님 <span>외 10명</span>이 좋아합니다.
+                </p>
+              </div>
+              <div style={{ marginTop: '10px' }} className="comment">
+                <div className="commentblock">
+                  <div>
+                    <div>
+                      <p className="nickname">neceosecius</p>
+                      <span>거봐 좋았잖아~~~~🙆🏻‍♀️</span>
+                    </div>
+                  </div>
+                  <div>
+                    <i className="far fa-heart"></i>
+                  </div>
+                </div>
+                <div className="commentblock">
+                  <div>
+                    <p className="nickname">neceosecius</p>
+                    <span>거봐 좋았잖아~~~~🙆🏻‍♀️</span>
+                  </div>
+                  <div>
+                    <i className="far fa-heart"></i>
+                  </div>
+                </div>
+              </div>
+              <p className="aftertime">42분 전</p>
+              <div className="commentplace">
+                <input
+                  id="comment"
+                  type="text"
+                  name="댓글"
+                  placeholder="댓글 달기..."
+                />
+                <button className="gocomment">게시</button>
+              </div>
+            </div>
+          </article>
+
+          <div className="main-right">
+            <div className="myproflie">
+              <div className="myimage">
+                <img src="https://images.unsplash.com/photo-1644307358784-c6c589c9dcac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1273&q=80" />
+              </div>
+              <div className="myname">
+                <h2 id="myname">suin_me</h2>
+                <p>WeCode | 위코드</p>
+              </div>
+            </div>
+            <div className="story right-box">
+              <div className="box-title">
+                <p>스토리</p>
+                <p>모두 보기</p>
+              </div>
+              <div className="box-content">
+                <img src="https://images.unsplash.com/photo-1644307358784-c6c589c9dcac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=673&q=80" />
+                <div>
+                  <h3>이름</h3>
+                  <p>몇 분전</p>
+                </div>
+              </div>
+              <div className="box-content">
+                <img src="https://images.unsplash.com/photo-1644308414681-709b417832b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+                <div>
+                  <h3>이름</h3>
+                  <p>몇 분전</p>
+                </div>
+              </div>
+              <div className="box-content">
+                <img src="https://images.unsplash.com/photo-1644308414681-709b417832b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+                <div>
+                  <h3>이름</h3>
+                  <p>몇 분전</p>
+                </div>
+              </div>
+              <div className="box-content">
+                <img src="https://images.unsplash.com/photo-1644308414681-709b417832b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+                <div>
+                  <h3>이름</h3>
+                  <p>몇 분전</p>
+                </div>
+              </div>
+              <div className="box-content">
+                <img src="https://images.unsplash.com/photo-1644308414681-709b417832b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+                <div>
+                  <h3>이름</h3>
+                  <p>몇 분전</p>
+                </div>
+              </div>
+              <div className="box-content">
+                <img src="https://images.unsplash.com/photo-1644308414681-709b417832b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+                <div>
+                  <h3>이름</h3>
+                  <p>몇 분전</p>
+                </div>
+              </div>
+            </div>
+            <div className="recommend right-box">
+              <div className="box-title">
+                <p>회원님을 위한 추천</p>
+                <p>모두 보기</p>
+              </div>
+              <div className="box-content">
+                <img src="https://images.unsplash.com/photo-1644308414681-709b417832b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+                <div>
+                  <h3>이름</h3>
+                  <p>몇 분전</p>
+                </div>
+                <p className="follow">팔로우</p>
+              </div>
+              <div className="box-content">
+                <img src="https://images.unsplash.com/photo-1644308414681-709b417832b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+                <div>
+                  <h3>이름</h3>
+                  <p>몇 분전</p>
+                </div>
+                <p className="follow">팔로우</p>
+              </div>
+              <div className="box-content">
+                <img src="https://images.unsplash.com/photo-1644308414681-709b417832b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+                <div>
+                  <h3>이름</h3>
+                  <p>몇 분전</p>
+                </div>
+                <p className="follow">팔로우</p>
+              </div>
+            </div>
+            <div className="footer">
+              <p>
+                instagram 정보 | 지원 | 홍보센터 | API
+                <br />
+                채용 정보 | 개인정보처리방침 | 약관
+                <br />
+                디렉터리 | 프로필 | 해시태그 | 언어
+              </p>
+              <br />
+              <p>&copy; 2019 INSTAGRAM</p>
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default MainJung;
