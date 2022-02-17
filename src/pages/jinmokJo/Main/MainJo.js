90079363a01e7384f1d561e4ef99f7e62df59d0e
ChoJinmok <jjmok1127@gmail.com>
2022-02-17 16:56:13 +0900

    Rename

--- src/pages/jinmokJo/Main/MainJo.js
+++ src/pages/jinmokJo/Main/MainJo.js
@@ -90,6 +90,87 @@ function MainJo() {
             </form>
           </div>
         </article>
+        <article className="feedArticle">
+          <div className="articleHeader">
+            <div className="profiles feeds_my_id">
+              <a href="#" className="profile-img">
+                <span>&gt;wecode</span>
+              </a>
+              <div className="id_text">
+                <span className="bold">wecode_bootcamp</span>
+                <span>WeCode - 위코드</span>
+              </div>
+            </div>
+            <a className="article-header-menu" href="#">
+              <i className="fas fa-ellipsis-h"></i>
+            </a>
+          </div>
+          <div className="contents"></div>
+          <div className="article-bottom">
+            <div className="article-btns">
+              <div>
+                <button className="heartButton hover" onClick={console.log()}>
+                  <i className="far fa-heart"></i>
+                </button>
+                <button className="hover">
+                  <i className="far fa-comment"></i>
+                </button>
+                <button className="hover">
+                  <i className="fas fa-share-square"></i>
+                </button>
+              </div>
+              <div>
+                <button
+                  className="bookmarkButton hover"
+                  onClick={console.log()}
+                >
+                  <i className="far fa-bookmark"></i>
+                </button>
+              </div>
+            </div>
+            <div className="like-section">
+              <div className="like-profile"></div>
+              <span>
+                <span className="like-representive bold">seungyoun_iain</span>님
+                외 <span className="like-people bold">4명</span>이 좋아합니다
+              </span>
+            </div>
+            <div className="article-text">
+              <span>
+                <span className="bold">wecode_bootcamp</span>
+                "위코드는 단순 교육업체가 아닌 개발자 커뮤니티입니다. Wecode에서
+                배우고 저는 총 5개 회사에서 오퍼를 받았습니다." - Wecode 졸업생
+                김병진님 인터뷰 -
+              </span>
+            </div>
+            <button className="reply">댓글</button>
+            <div className="upload-time">54분 전</div>
+          </div>
+          <div className="comments">
+            <form id="comments-form" method="post" action="">
+              <button className="emoji" type="button">
+                <i className="far fa-smile"></i>
+              </button>
+              <textarea
+                name="comments-area"
+                id="comments-area"
+                placeholder="댓글 달기..."
+                autoComplete="off"
+                autoCorrect="off"
+                onKeyDown={console.log()}
+                onKeyUp={console.log()}
+              ></textarea>
+              <button
+                id="comments-submit"
+                type="submit"
+                form="comments-form"
+                disabled
+              >
+                게시
+              </button>
+            </form>
+          </div>
+        </article>
       </main>
       <aside id="story-recommand">
         <div className="story-recommand-section">
