aa2965059d125e5a12c6c86c01e2e61e573c4152
전슬기 <jeonseulgi@jeonseulgiui-MacBook-Air.local>
2022-02-18 17:11:30 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/seulgiJun/Login/LoginJun.js
+++ src/pages/seulgiJun/Login/LoginJun.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import './LoginJun.scss';
 
 function LoginJun() {
   const navigator = useNavigate();
+  const [idValue, setIdValue] = useState('');
+  const [pwValue, setPwValue] = useState('');
 
   const goToMain = () => {
-    navigator('/main');
+    navigator('/main-jun');
+  };
+
+  const handledIdInput = event => {
+    setIdValue(event.target.value);
+  };
+  const handledPwInput = event => {
+    setPwValue(event.target.value);
   };
 
   return (
@@ -15,11 +24,18 @@ function LoginJun() {
       <div className="login">
         <h1>Westagram</h1>
         <form className="loginForm">
-          <input placeholder="전화번호, 사용자 이름 또는 이메일"></input>
-          <input type="password" placeholder="비밀번호" />
+          <input
+            onChange={handledIdInput}
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+          <input
+            onChange={handledPwInput}
+            type="password"
+            placeholder="비밀번호"
+          />
           <button onClick={goToMain}>로그인</button>
         </form>
-        <a href="">비밀번호를 잊으셨나요?</a>
+        <a href="#">비밀번호를 잊으셨나요?</a>
       </div>
     </main>
   );
