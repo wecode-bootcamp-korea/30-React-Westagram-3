685df20b5447aa0455ea711c9d54ac87a8542635
ChoJinmok <jjmok1127@gmail.com>
2022-02-20 21:32:31 +0900

    Modify : inputValue state를 객체로 변경 후 userId와 password 같이 담음, Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/jinmokJo/Login/LoginJo.js
+++ src/pages/jinmokJo/Login/LoginJo.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import LoginInput from './Components/LoginInput';
 
 import './LoginJo.scss';
 
 function LoginJo() {
+  const [inputValue, setInputValue] = useState({ userId: '', password: '' });
+
+  const handleLoginInput = e => {
+    if (e.target.type === 'text') {
+      setInputValue(prevState => ({
+        userId: e.target.value,
+        password: inputValue.password,
+      }));
+    } else if (e.target.type === 'password') {
+      setInputValue(prevState => ({
+        userId: inputValue.userId,
+        password: e.target.value,
+      }));
+    }
+  };
+
   return (
     <main className="Login">
       <div className="loginMain">
         <h1 className="instagramFont title">Westagram</h1>
         <form action="" method="post" id="loginForm">
           <LoginInput
-            placeholder="전화번호, 사용자 이름 또는 이메일"
+            placeholder="이메일"
             type="text"
+            inputValue={inputValue}
+            handleIdInput={handleLoginInput}
           />
-          <LoginInput placeholder="비밀번호" type="password" name="password" />
+          <LoginInput
+            placeholder="비밀번호"
+            type="password"
+            name="password"
+            inputValue={inputValue}
+            handleIdInput={handleLoginInput}
+          />
           <button
             type="submit"
             form="loginForm"
-            className="loginBtn"
-            onClick={console.log('와 햇갈리네')}
-            disabled
+            className={`loginBtn ${
+              inputValue.userId.includes('@') && inputValue.password.length >= 5
+                ? 'activeLoginBtn'
+                : ''
+            }`}
+            disabled={
+              inputValue.userId.includes('@') && inputValue.password.length >= 5
+                ? false
+                : true
+            }
           >
             로그인
           </button>
