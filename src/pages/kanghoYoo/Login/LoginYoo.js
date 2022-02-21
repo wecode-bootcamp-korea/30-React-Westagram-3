8c9dda63f1a463d1371a894e0ab956222bf288ad
kangho <kangsick98@gmail.com>
2022-02-21 13:17:47 +0900

    Add: Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/kanghoYoo/Login/LoginYoo.js
+++ src/pages/kanghoYoo/Login/LoginYoo.js
@@ -1,37 +1,44 @@
 import React from 'react';
 import './LoginYoo.scss';
 import { Link } from 'react-router-dom';
-import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
-import Nav from '../../../components/Nav/Nav';
+import { useState } from 'react';
+
 function LoginYoo() {
-  // function handleSubmit(e) {
-  //   const
-  // }
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+
+  function handleIdInput(e) {
+    setId(e.target.value);
+  }
+
+  function handlePwInput(e) {
+    setPw(e.target.value);
+  }
 
   const navigate = useNavigate();
   const goToMain = () => {
-    navigate('/main');
+    navigate('/main-Yoo');
   };
 
   return (
     <div className="login-js">
-      <Nav />
       <main className="wrapClass">
         <span className="lobsterFont">Westagram</span>
         <div className="inputWrap">
           <input
-            onChange={handleldInput}
+            onChange={handleIdInput}
             type="text"
             className="id input"
             placeholder="전화번호, 사용자 이름 또는 이메일"
-            value=""
+            value={id}
           />
           <input
+            onChange={handlePwInput}
             type="password"
             className="pw input"
             placeholder="비밀번호"
-            value=""
+            value={pw}
           />
         </div>
         <div className="bt">
